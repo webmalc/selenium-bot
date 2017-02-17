5700479794aeddabe46bdfad44b63e10ed2c4295
webmalc <webmalc@gmail.com>
2017-02-17 18:51:26 +0300

    500px workflow

--- 500px.py
+++ 500px.py
@@ -12,12 +12,12 @@ import settings
 class App(object):
 
     def __init__(self):
-        self.driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+        self.driver = webdriver.Chrome(settings.DRIVER_PATH)
 
     def _auth(self):
         self.driver.get('https://500px.com/login')
-        self.driver.find_element_by_name('email').send_keys(settings.USERNAME)
-        self.driver.find_element_by_name('password').send_keys(settings.PASS)
+        self.driver.find_element_by_name('email').send_keys(settings.USERNAME_500PX)
+        self.driver.find_element_by_name('password').send_keys(settings.PASS_500PX)
         self.driver.find_element_by_css_selector('input.unified_signup__submit_button').click()
         time.sleep(5)
         self.driver.get('https://500px.com/')
@@ -27,27 +27,33 @@ class App(object):
             EC.presence_of_element_located((By.CSS_SELECTOR, "a.new_fav"))
         )
 
-    def _like(self, skip=False, iterations=50):
+    def _like(self, skip=False, iterations=3):
         self._wait_for_links()
         counter = 0
+
         for i in range(0, iterations):
-            for link in self.driver.find_elements_by_css_selector('a.new_fav:not(.hearted)'):
-                try:
-                    if not skip or (skip and random.randrange(1, 11) > 3):
-                        link.click()
-                    counter += 1
-                    time.sleep(random.randrange(1, 3))
-                except:
-                    pass
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            self._wait_for_links()
+            time.sleep(1)
+
+        for link in self.driver.find_elements_by_css_selector('a.new_fav:not(.hearted)'):
+            try:
+                if not skip or (skip and random.randrange(1, 11) > 3):
+                    link.click()
+                counter += 1
+                time.sleep(random.randrange(1, 3))
+            except:
+                pass
 
         print('Total: {}'.format(counter))
 
     def run(self):
         # following
         self._auth()
-        self._like(iterations=150)
+        self._like()
+
+        # activity
+        self.driver.get('https://500px.com/activity')
+        self._like(skip=True)
 
         # fresh
         self.driver.get('https://500px.com/fresh')
